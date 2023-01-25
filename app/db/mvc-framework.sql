c77970e6a30deb612951bd8209b8d4472bc43917
Abdullatif Ahmad <abdullatif.ahmad08082002@gmail.com>
2023-01-25 11:45:17 +0100

    new database

--- app/db/mvc-framework.sql
+++ app/db/mvc-framework.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Gegenereerd op: 04 okt 2022 om 06:47
--- Serverversie: 5.7.31
--- PHP-versie: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,16 +18,16 @@ USE `mvc-2109a`;
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `country`
+-- Tabelstructuur voor tabel `voertuig`
 --
 
-DROP TABLE IF EXISTS `voertuig`;
-CREATE TABLE IF NOT EXISTS `voertuig` (
+DROP TABLE IF EXISTS `Voertuig`;
+CREATE TABLE IF NOT EXISTS `Voertuig` (
   `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Kenteken` varchar(8) NOT NULL,
+  `Kenteken` varchar(20) NOT NULL,
   `Type` varchar(100) NOT NULL,
-  `Bouwjaar` date NOT NULL,
-  `Brandstof` varchar(10) NOT NULL,
+  `Bouwjaar` date() NOT NULL,
+  `Brandstof` varchar(20) UNSIGNED NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
@@ -56,35 +48,11 @@ INSERT INTO `Voertuig` (`Kenteken`, `Type`, `Bouwjaar`, `Brandstof`) VALUES
 ('DRS-52-P', 'Vespa', '2022-03-21', 'Benzine'),
 ('STP-12-U', 'Kymco', '2022-07-02', 'Benzine'),
 ('45-SD-23', 'Renault', '2023-01-01', 'Diesel'),
-COMMIT;
+
+
+-- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `VoertuigInstructeur`
---
-
-DROP TABLE IF EXISTS `VoertuigInstructeur`;
-CREATE TABLE IF NOT EXISTS `VoertuigInstructeur` (
-  `Id` smallint(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `VoertuigId` int(10) NOT NULL,
-  `InstructeurId` int(10) NOT NULL,
-  `DatumToekenning` date NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
-
---
--- Gegevens worden geëxporteerd voor tabel `VoertuigInstructeur`
---
-
-INSERT INTO `VoertuigInstructeur` (`VoertuigId`, `InstructeurId`, `DatumToekenning`) VALUES
-('1', '5', '2017-06-18'),
-('3', '1', '2021-09-26'),
-('9', '1', '2021-09-27'),
-('3', '4', '2022-08-01'),
-('5', '1', '2019-08-30'),
-('10', '5', '2020-02-02'),
-
-COMMIT;
-
 -- Tabelstructuur voor tabel `TypeVoertuig`
 --
 
@@ -105,6 +73,120 @@ INSERT INTO `TypeVoertuig` (`TypeVoertuig`, `RijbewijsCategorie`) VALUES
 ('Vrachtwagen', 'C'),
 ('Bus', 'D'),
 ('Bromfiets', 'AM');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Leerling`
+--
+
+DROP TABLE IF EXISTS `Instructeur`;
+CREATE TABLE IF NOT EXISTS `Instructeur` (
+  `Id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Voornaam` varchar(100) NOT NULL,
+  `Tussenvoegsel` varchar(10) NULL,
+  `Achternaam`varchar(50) NO NULL,
+  `Mobiel` varchar(50) NO NULL,
+  `DatumInDienst` Date NO NULL,
+  `AantalSterren` varchar(5) NO NULL
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Leerling`
+--
+
+INSERT INTO `Instructeur` (`Voornaam`, `Tussenvoegsel`, `Achternaam`, `Mobiel`, `DatumInDienst`, `AantalSterren`) VALUES
+(3, 'Konijn'),
+(4, 'Slavink'),
+(6, 'Otto');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Les`
+--
+
+DROP TABLE IF EXISTS `Les`;
+CREATE TABLE IF NOT EXISTS `Les` (
+  `Id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `DatumTijd` datetime NOT NULL,
+  `LeerlingId` mediumint(8) UNSIGNED NOT NULL,
+  `InstructeurId` tinyint(3) UNSIGNED NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_Les_LeerlingId_Leerling_Id` (`LeerlingId`),
+  KEY `FK_Les_InstructeurId_Instructeur_Id` (`InstructeurId`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `Les`
+--
+
+INSERT INTO `Les` (`Id`, `DatumTijd`, `LeerlingId`, `InstructeurId`) VALUES
+(45, '2022-05-20 14:00:00', 3, 1),
+(46, '2022-05-20 10:00:00', 6, 3),
+(47, '2022-05-21 13:00:00', 4, 2),
+(48, '2022-05-21 17:00:00', 6, 3),
+(49, '2022-05-22 11:00:00', 3, 1),
+(50, '2022-05-28 19:00:00', 4, 2),
+(51, '2022-06-01 20:00:00', 3, 2),
+(52, '2022-06-12 08:00:00', 3, 1),
+(53, '2022-06-22 12:00:00', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Onderwerp`
+--
+
+DROP TABLE IF EXISTS `Onderwerp`;
+CREATE TABLE IF NOT EXISTS `Onderwerp` (
+  `Id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `LesId` mediumint(8) UNSIGNED NOT NULL,
+  `Onderwerp` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_Onderwerp_LesId_Les_Id` (`LesId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `Opmerking`
+--
+
+DROP TABLE IF EXISTS `Opmerking`;
+CREATE TABLE IF NOT EXISTS `Opmerking` (
+  `Id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `LesId` mediumint(8) UNSIGNED NOT NULL,
+  `Opmerking` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_Opmerking_LesId_Les_Id` (`LesId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `Les`
+--
+ALTER TABLE `Les`
+  ADD CONSTRAINT `FK_Les_InstructeurId_Instructeur_Id` FOREIGN KEY (`InstructeurId`) REFERENCES `Instructeur` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_Les_LeerlingId_Leerling_Id` FOREIGN KEY (`LeerlingId`) REFERENCES `Leerling` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `Onderwerp`
+--
+ALTER TABLE `Onderwerp`
+  ADD CONSTRAINT `FK_Onderwerp_LesId_Les_Id` FOREIGN KEY (`LesId`) REFERENCES `Les` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `Opmerking`
+--
+ALTER TABLE `Opmerking`
+  ADD CONSTRAINT `FK_Opmerking_LesId_Les_Id` FOREIGN KEY (`LesId`) REFERENCES `Les` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
